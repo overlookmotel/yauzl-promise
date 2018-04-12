56db43b42aaf6e7b4b790c0970bfdabf296b5baa
overlookmotel <theoverlookmotel@gmail.com>
2018-04-12 03:41:50 +0100

    Initial release

--- README.md
+++ README.md
@@ -13,6 +13,125 @@
 
 ## Usage
 
+Promisified version of [yauzl](https://www.npmjs.com/package/yauzl) for unzipping ZIP files.
+
+### Installation
+
+```
+npm install yauzl-promise
+```
+
+### Methods
+
+#### `open()` / `fromFd()` / `fromBuffer()` / `fromRandomAccessReader()`
+
+These methods all work as before, but return a Promise rather than taking a callback.
+
+```js
+const yauzl = require( 'yauzl-promise' );
+
+const zipFile = await yauzl.open( '/path/to/file' );
+```
+
+NB The `lazyEntries` option is automatically enabled. Get file entries using methods listed below.
+
+#### `zipFile.readEntry()`
+
+Same as original yauzl method, but returning a promise. Promise resolves to an instance of `yauzl.Entry`, or rejects if there is an error.
+
+```js
+const entry = await zipFile.readEntry();
+console.log( entry );
+```
+
+Calling `.readEntry()` again returns the next entry. When there are no entries left, it returns `null`.
+
+#### `zipFile.readEntries( [numEntries] )`
+
+Read several entries and return as an array.
+
+```js
+const entries = await zipFile.readEntries( 3 );
+entries.forEach( console.log );
+```
+
+If `numEntries` is `0`, `null` or `undefined`, reading will continue until all entries are read.
+
+WARNING: This is dangerous. If ZIP contains a large number of files, could lead to crash due to out of memory.
+
+#### `zipFile.walkEntries( callback )`
+
+Read all entries and call `callback` for each.
+
+Returns a promise which resolves when all have been read.
+
+```js
+await zipFile.walkEntries( entry => {
+	console.log( entry );
+} );
+console.log( 'Done' );
+```
+
+#### `zipFile.openReadStream( entry [, options] )`
+
+Same as original method but returns promise of a stream.
+
+```js
+const readStream = await zipFile.openReadStream( entry );
+readStream.pipe( writeStream );
+```
+
+#### `entry.openReadStream( [options] )`
+
+As above, but called on an `Entry` object.
+
+```js
+const entry = await zipFile.readEntry();
+const readStream = await entry.openReadStream();
+readStream.pipe( writeStream );
+```
+
+### Customization
+
+#### Alternative Promise implementation
+
+Promises returned by default are native JS Promises.
+
+`.usePromise()` returns a new `yauzl` object where the methods return promises from the specified Promise constructor.
+
+```js
+const Bluebird = require( 'bluebird' );
+const yauzl = require( 'yauzl-promise' ).usePromise( Bluebird );
+
+const p = yauzl.open( '/path/to/file' );
+console.log( p instanceof Bluebird ); // true
+```
+
+NB This does not alter the original `yauzl` object, only the one returned from `.usePromise()`.
+
+```js
+const Bluebird = require( 'bluebird' );
+const yauzl = require( 'yauzl-promise' )
+const yauzlBluebird = yauzl.usePromise( Bluebird );
+
+const p = yauzl.open( '/path/to/file' );
+console.log( p instanceof Bluebird ); // false
+
+const p = yauzlBluebird.open( '/path/to/file' );
+console.log( p instanceof Bluebird ); // true
+```
+
+#### Using another version of yauzl
+
+`.useYauzl()` method promisifies a specific `yauzl` object.
+
+Only useful if you have a modified version of yauzl which you want to promisify.
+
+```js
+const yauzlFork = require( 'my-yauzl-fork' );
+const yauzl = require( 'yauzl-promise' ).useYauzl( yauzlFork );
+```
+
 ## Tests
 
 Use `npm test` to run the tests. Use `npm run cover` to check coverage.
--- test/all.test.js
+++ test/all.test.js
@@ -8,6 +8,10 @@
 // Modules
 const chai = require('chai'),
 	expect = chai.expect,
+	pathJoin = require('path').join,
+	{EventEmitter} = require('events'),
+	ReadableStream = require('stream').Readable,
+	yauzlOriginal = require('yauzl'),
 	yauzl = require('../lib/');
 
 // Init
@@ -16,10 +20,216 @@ chai.config.includeStack = true;
 // Tests
 
 /* jshint expr: true */
-/* global describe, it */
+/* global describe, it, beforeEach, afterEach */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(yauzl).to.be.ok;
+const PATH = pathJoin(__dirname, 'test.zip'),
+	FILES = ['test_files/', 'test_files/1.txt', 'test_files/2.txt', 'test_files/3.txt'];
+
+describe('Module', function() {
+	it('clones yauzl object', function() {
+		expect(yauzl).to.not.equal(yauzlOriginal);
+	});
+
+	it('clones yauzl.ZipFile', function() {
+		const {ZipFile} = yauzl;
+		expect(ZipFile).to.not.equal(yauzlOriginal.ZipFile);
+
+		const zipFile = Object.create(ZipFile.prototype);
+		expect(zipFile).to.be.instanceof(ZipFile);
+		expect(zipFile).not.to.be.instanceof(yauzlOriginal.ZipFile);
+		expect(zipFile).to.be.instanceof(EventEmitter);
+	});
+
+	it('clones yauzl.Entry', function() {
+		const {Entry} = yauzl;
+		expect(Entry).to.not.equal(yauzlOriginal.Entry);
+
+		const entry = Object.create(Entry.prototype);
+		expect(entry).to.be.instanceof(Entry);
+		expect(entry).not.to.be.instanceof(yauzlOriginal.Entry);
+	});
+});
+
+describe('.open()', function() {
+	it('returns a Promise', function() {
+		const promise = yauzl.open(PATH);
+		expect(promise).to.be.instanceof(Promise);
+		return promise.then(zipFile => {
+			zipFile.close();
+		});
+	});
+
+	it('resolves to instance of yauzl.ZipFile', function() {
+		return yauzl.open(PATH).then(zipFile => {
+			expect(zipFile).to.be.instanceof(yauzl.ZipFile);
+			zipFile.close();
+		});
+	});
+
+	it('ignores `lazyEntries` option', function() {
+		return yauzl.open(PATH, {lazyEntries: false}).then(zipFile => {
+			expect(zipFile.lazyEntries).to.equal(true);
+			zipFile.close();
+		});
+	});
+});
+
+describe('Entry methods', function() {
+	beforeEach(function() {
+		return yauzl.open(PATH).then(zipFile => {
+			this.zipFile = zipFile;
+		});
+	});
+
+	afterEach(function() {
+		this.zipFile.close();
+	});
+
+	describe('.readEntry()', function() {
+		beforeEach(function() {
+			this.promise = this.zipFile.readEntry();
+			return this.promise.then(entry => {
+				this.entry = entry;
+			});
+		});
+
+		it('returns a Promise', function() {
+			expect(this.promise).to.be.instanceof(Promise);
+		});
+
+		it('resolves to instance of yauzl.Entry', function() {
+			expect(this.entry).to.be.instanceof(yauzl.Entry);
+		});
+
+		it('returns first entry', function() {
+			expect(this.entry.fileName).to.equal(FILES[0]);
+		});
+
+		it('when called again, returns next entry', function() {
+			return this.zipFile.readEntry().then(entry => {
+				expect(entry.fileName).to.equal(FILES[1]);
+			});
+		});
+
+		it('returns `null` when all entries consumed', function() {
+			expect(this.entry.fileName).to.equal(FILES[0]);
+			return this.zipFile.readEntry().then(entry => {
+				expect(entry.fileName).to.equal(FILES[1]);
+				return this.zipFile.readEntry();
+			}).then(entry => {
+				expect(entry.fileName).to.equal(FILES[2]);
+				return this.zipFile.readEntry();
+			}).then(entry => {
+				expect(entry.fileName).to.equal(FILES[3]);
+				return this.zipFile.readEntry();
+			}).then(entry => {
+				expect(entry).to.be.null;
+			});
+		});
+	});
+
+	describe('.readEntries()', function() {
+		it('returns a Promise', function() {
+			const promise = this.zipFile.readEntries();
+			expect(promise).to.be.instanceof(Promise);
+			return promise;
+		});
+
+		it('returns array of `numEntries` entries', function() {
+			return this.zipFile.readEntries(2).then(entries => {
+				expect(entries).to.be.an('array');
+				expect(entries).to.have.lengthOf(2);
+				const fileNames = entries.map(entry => entry.fileName);
+				expect(fileNames).to.deep.equal(FILES.slice(0, 2));
+			});
+		});
+
+		it('when called again, returns next entries', function() {
+			return this.zipFile.readEntries(2).then(() => {
+				return this.zipFile.readEntries(2);
+			}).then(entries => {
+				expect(entries).to.be.an('array');
+				expect(entries).to.have.lengthOf(2);
+				const fileNames = entries.map(entry => entry.fileName);
+				expect(fileNames).to.deep.equal(FILES.slice(2, 4));
+			});
+		});
+
+		it('with no `numEntries` specified, returns all entries', function() {
+			return this.zipFile.readEntries().then(entries => {
+				expect(entries).to.be.an('array');
+				expect(entries).to.have.lengthOf(FILES.length);
+				const fileNames = entries.map(entry => entry.fileName);
+				expect(fileNames).to.deep.equal(FILES);
+			});
+		});
+	});
+
+	describe('.walkEntries()', function() {
+		it('returns a Promise', function() {
+			const promise = this.zipFile.walkEntries(() => {});
+			expect(promise).to.be.instanceof(Promise);
+			return promise;
+		});
+
+		it('calls callback for each entry', function() {
+			const entries = [];
+			return this.zipFile.walkEntries(entry => {
+				entries.push(entry);
+			}).then(() => {
+				const fileNames = entries.map(entry => entry.fileName);
+				expect(fileNames).to.deep.equal(FILES);
+			});
+		});
+	});
+});
+
+describe('Stream methods', function() {
+	beforeEach(function() {
+		return yauzl.open(PATH).then(zipFile => {
+			this.zipFile = zipFile;
+		});
+	});
+
+	afterEach(function() {
+		this.zipFile.close();
+	});
+
+	describe('zipFile.openReadStream()', function() {
+		beforeEach(function() {
+			this.promise = this.zipFile.readEntry().then(entry => {
+				return this.zipFile.openReadStream(entry);
+			});
+			return this.promise.then(stream => {
+				this.stream = stream;
+			});
+		});
+
+		it('returns a Promise', function() {
+			expect(this.promise).to.be.instanceof(Promise);
+		});
+
+		it('resolves to Readable Stream', function() {
+			expect(this.stream).to.be.instanceof(ReadableStream);
+		});
+	});
+
+	describe('entry.openReadStream()', function() {
+		beforeEach(function() {
+			this.promise = this.zipFile.readEntry().then(entry => {
+				return entry.openReadStream();
+			});
+			return this.promise.then(stream => {
+				this.stream = stream;
+			});
+		});
+
+		it('returns a Promise', function() {
+			expect(this.promise).to.be.instanceof(Promise);
+		});
+
+		it('resolves to Readable Stream', function() {
+			expect(this.stream).to.be.instanceof(ReadableStream);
+		});
 	});
 });
