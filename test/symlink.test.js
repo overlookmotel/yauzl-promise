28443949fa0cd7a2881b7b52d3b61f7676b69d6e
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-05-10 17:59:43 +0530

    Commit symlink fixture to repo and remove Archiver package

--- test/symlink.test.js
+++ test/symlink.test.js
@@ -9,52 +9,30 @@
 require('./support/index.js');
 
 // Modules
-const fs = require('fs'),
-	path = require('path'),
-	archiver = require('archiver'),
+const path = require('path'),
 	yauzl = require('yauzl-promise');
 
-describe('symlink', () => {
-	const symlinkFixtureDirPath = path.resolve('test', 'fixtures', 'symlink');
-	const filePath = path.resolve(symlinkFixtureDirPath, 'file.txt');
-	const symlinkPath = path.resolve(symlinkFixtureDirPath, 'symlink_to_file.txt');
-	const zipPath = path.resolve(symlinkFixtureDirPath, 'symlink.zip');
+describe('Identify symlinks', () => {
+	const zipPath = path.resolve('test', 'fixtures', 'symlink', 'node_generated.zip');
 
-	beforeAll(async () => {
-		/* Create file and symlink */
-		await fs.promises.mkdir(symlinkFixtureDirPath, { recursive: true });
-		await fs.promises.writeFile(filePath, 'some text');
-		await fs.promises.symlink(filePath, symlinkPath);
-
-		/* Create zip file */
-		const writeStream = fs.createWriteStream(zipPath);
-		const archive = archiver('zip');
-		archive.pipe(writeStream);
-		archive.file(filePath, { name: 'file.txt' });
-		archive.file(symlinkPath, { name: 'symlink_to_file.txt' });
-		await archive.finalize();
-	});
-
-	it('unzip, read and check if any entry is a symlink', async () => {
+	it('unzips, reads and checks if any entry is a symlink', async () => {
 		const zip = await yauzl.open(zipPath);
-		let entry = await zip.readEntry();
 		const zipMetadata = [];
-		while (entry !== null) {
-			zipMetadata.push({
-				fileName: entry.filename,
-				isSymlink: await entry.isSymlink(),
-			});
-			entry = await zip.readEntry();
+
+		try {
+			for await (const entry of zip) {
+				zipMetadata.push({
+					fileName: entry.filename,
+					isSymlink: entry.isSymlink()
+				});
+			}
+		} finally {
+			zip.close();
 		}
-		await zip.close();
 
 		expect(zipMetadata).toEqual([
-			{ fileName: 'file.txt', isSymlink: false },
-			{ fileName: 'symlink_to_file.txt', isSymlink: true },
+			{fileName: 'symlink_to_file.txt', isSymlink: true},
+			{fileName: 'file.txt', isSymlink: false}
 		]);
-	});
-
-	afterAll(async () => {
-		await fs.promises.rm(symlinkFixtureDirPath, { recursive: true, force: true });
 	});
 });
