6a4dd8c14418b865285e44c66a997abdcd8e798b
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-03 09:01:36 +0530

    Add failing WinZip test

--- test/symlink.test.js
+++ test/symlink.test.js
@@ -12,10 +12,11 @@ require('./support/index.js');
 const path = require('path'),
 	yauzl = require('yauzl-promise');
 
+/* It unzips, reads and checks if any entry is a symlink. */
 describe('Identify symlinks', () => {
-	const zipPath = path.resolve('test', 'fixtures', 'symlink', 'Node generated text file with symlink.zip');
 
-	it('unzips, reads and checks if any entry is a symlink', async () => {
+	it('reads symlink from NodeJS generated zip file', async () => {
+		const zipPath = path.resolve('test', 'fixtures', 'symlink', 'NodeJS_text_file_with_symlink.zip');
 		const zip = await yauzl.open(zipPath);
 		const zipMetadata = [];
 
@@ -33,6 +34,29 @@ describe('Identify symlinks', () => {
 		expect(zipMetadata).toEqual([
 			{fileName: 'symlink_to_file.txt', isSymlink: true},
 			{fileName: 'file.txt', isSymlink: false}
+		]);
+	});
+
+	it('reads symlink from WinZip generated zip file', async () => {
+		const zipPath = path.resolve('test', 'fixtures', 'symlink', 'WinZip_text_file_with_symlink.zip');
+		const zip = await yauzl.open(zipPath);
+		const zipMetadata = [];
+
+		try {
+			for await (const entry of zip) {
+				zipMetadata.push({
+					fileName: entry.filename,
+					isSymlink: entry.isSymlink()
+				});
+			}
+		} finally {
+			zip.close();
+		}
+
+		expect(zipMetadata).toEqual([
+			{fileName: 'winzip/', isSymlink: false},
+			{fileName: 'winzip/file.txt', isSymlink: false},
+			{fileName: 'winzip/file_symlink.lnk', isSymlink: true}
 		]);
 	});
 });
