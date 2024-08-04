f53b5b7152a1335b658f9322f64544f1f725e752
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-04 18:56:01 +0530

    Add failing test for MacOS Archive Utility

--- test/symlink.test.js
+++ test/symlink.test.js
@@ -105,4 +105,30 @@ describe('Identify symlinks', () => {
 			{fileName: 'winrar/file_symlink.lnk', isSymlink: true}
 		]);
 	});
+
+	it('reads symlink from MacOS Archive Utility generated zip file', async () => {
+		const zipPath = path.resolve('test', 'fixtures', 'symlink', 'MacOS_Archive_Utility_text_with_symlink.zip');
+		const zip = await yauzl.open(zipPath);
+		const zipMetadata = [];
+
+		try {
+			for await (const entry of zip) {
+				zipMetadata.push({
+					fileName: entry.filename,
+					isSymlink: entry.isSymlink()
+				});
+			}
+		} finally {
+			zip.close();
+		}
+
+		expect(zipMetadata).toEqual([
+			{fileName: 'MacOS Archive Utility/', isSymlink: false},
+			{fileName: 'MacOS Archive Utility/file.txt', isSymlink: false},
+			{fileName: 'MacOS Archive Utility/.DS_Store', isSymlink: false},
+			{fileName: '__MACOSX/MacOS Archive Utility/._.DS_Store', isSymlink: false},
+			{fileName: 'MacOS Archive Utility/file.txt alias', isSymlink: true},
+			{fileName: '__MACOSX/MacOS Archive Utility/._file.txt alias', isSymlink: true},
+		]);
+	});
 });
